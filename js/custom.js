7b38c4ddd1224b2dee3e0c81cc67bab6b4f94adf
gurpreetweb <gopysingh1987@gmail.com>
2020-01-23 12:39:40 +0530

    factory function in javascript

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,37 @@
-for(i = 1; i <= 10; i++){
-    for(d = i; d <= 100; d+=10 ){
-        document.write(d + ' ');
+// for(i = 1; i <= 10; i++){
+//     for(d = i; d <= 100; d+=10 ){
+//         document.write(d + ' ');
+//     }
+//     document.write('<br>');
+// }
+// facotry function 
+
+// function mobile(){
+//     return{
+//       model: 'sumsung',
+//       price:function(){
+//           return 'price is 3000';
+//       }
+
+//     }
+// }
+// var sumsung = mobile();
+// document.write(sumsung.model + ' ' + sumsung.price());
+// document.write('<br>');
+// function newmobile(modelname,modelprice){
+//     document.write(modelname + ' ' + 'mobile price is ' + modelprice + '<br>' );
+// }
+function newmobile(modelname,modelprice){
+    return{
+      model: modelname,
+      price: modelprice
     }
-    document.write('<br>');
 }
+var sumsung = newmobile('sumsung',5000);
+var lg = newmobile('lg', 9000);
+var opo = newmobile('oppo', 11000);
+
+document.write(sumsung.model + ' ' + 'mobile price is ' + sumsung.price + '<br>' );
+document.write(lg.model + ' ' + 'mobile price is ' + lg.price + '<br>');
+document.write(opo.model + ' ' + 'mobile price is ' + opo.price );
+
